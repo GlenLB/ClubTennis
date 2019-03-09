1d72db9e65fdccd7838e87d346cb07b108e19988
Glen Le Baill <lebaill.glen@gmail.com>
2019-03-09 16:06:41 +0100

    Améliorations CSS

--- statics/js/global.js
+++ statics/js/global.js
@@ -2,15 +2,21 @@ window.onload = () => {
     // Déclaration du mode strict
     "use strict";
 
-    // Menu hamburger ----------------------
+    // Menu hamburger ------------------------------------------------------
+
     const menuHamburger = document.getElementById("menuHamburger");
     const liensNavMobile = document.getElementById("liensNavMobile");
+    const main = document.getElementsByTagName("main")[0];
     menuHamburger.onclick = () => {
         // Déclenche l'animation du menu mobile
         if (liensNavMobile.classList.contains("liensNavMobileHidden")) {
             liensNavMobile.classList.replace("liensNavMobileHidden", "liensNavMobileVisible");
+            //main.classList.add("blur");
+            main.style.opacity = "0.2";
         } else {
             liensNavMobile.classList.replace("liensNavMobileVisible", "liensNavMobileHidden");
+            //main.classList.remove("blur");
+            main.style.opacity = "1";
         }
         // Déclenche l'animation du menu hamburger
         if (menuHamburger.classList.contains("menuHamburger")) {
